0962c5252d1c008d53bc41547bd5cff8e57c2b47
Mauro Asprea <mauroasprea@gmail.com>
2020-06-01 02:08:50 +0200

    Remove Hash storage

--- lib/medusa/storage.rb
+++ lib/medusa/storage.rb
@@ -1,13 +1,5 @@
 module Medusa
   module Storage
-
-    def self.Hash(*args)
-      hash = Hash.new(*args)
-      # add close method for compatibility with Storage::Base
-      class << hash; def close; end; end
-      hash
-    end
-
     def self.Moneta(*args)
       require 'medusa/storage/moneta'
       self::Moneta.new(*args)
